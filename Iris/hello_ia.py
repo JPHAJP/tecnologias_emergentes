c0f32e01a3b880106d3c7103f920d80b8a0fd1ea
jpha <josepabloha@live.com.mx>
2024-08-28 11:44:57 -0600

    clase 3

--- Iris/hello_ia.py
+++ Iris/hello_ia.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+#ia liberias
+
+
 # Load the Iris dataset from a CSV file
 df = pd.read_csv('Iris\Iris.csv')
 df = df.drop('Id', axis=1)
